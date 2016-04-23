7eccf47268a660f5e3b5a4f5aa31665a75064ea7
shinya <shinshin930604@gmail.com>
2016-04-23 11:46:42 +0000

    three commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => "code-ganchan.c9users.io" }
+  config.action_mailer.default_url_options = { :host => "fathomless-wildwood-54724.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
