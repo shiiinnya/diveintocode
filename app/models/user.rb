890664beaa640c1cf9f692b787d98a4cb8bc9af1
shinya <shinshin930604@gmail.com>
2016-04-24 11:07:08 +0000

    owari

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  validates :username, presence: true, uniqueness: true
+
 end
