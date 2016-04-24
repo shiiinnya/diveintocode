7f677471980d1e727219ec5816d989057c0ac66e
shinya <shinshin930604@gmail.com>
2016-04-24 10:15:40 +0000

    finish!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+      def configure_permitted_parameters
+      # sign_inのときに、usernameも許可する
+      devise_parameter_sanitizer.for(:sign_in) << :username
+      # sign_upのときに、usernameも許可する
+      devise_parameter_sanitizer.for(:sign_up) << :username
+      #  account_updateのときに、usernameも許可する
+      devise_parameter_sanitizer.for(:account_update) << :username
+      end
+  
 end
